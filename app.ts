57cd76094cf588bcc5a25bb80a19b6fca3fdda46
RameenKhannnn <rameenkhan8787@gmail.com>
2024-03-17 22:55:48 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello,") + chalk.green("world"));
+console.log(chalk.red("HeLLO I'M A NEW,") + chalk.bold.yellow("PROGRAMMER"));
+console.log(chalk.yellow.bold("Hello Would you like to learn some TYPESCRIPT?"));
+console.log(chalk.red.underline("I love TYPESCRIPT."));
+
+
+
